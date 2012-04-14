28f56d86d5a7bc56a4bf11d2b163a7bf72007f5e
Tim Booher <ads@theboohers.org>
2012-04-14 07:44:45 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BooherLife::Application.config.secret_token = 'de9d05faddc5babe8d82eed846d0da2a7b2c51fbe3ccf3e789eb8a9dc7250d1897d5080353c91fca7f3d9d2a897de3a6a3b8b69b76a708aee14119b6b5bfdf6f'
