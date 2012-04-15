835c2f030a74f8783e4796496f0c7683b600aec8
Tim Booher <ads@theboohers.org>
2012-04-14 21:37:59 -0400

    added sliders

--- app/models/stressor.rb
+++ app/models/stressor.rb
@@ -2,5 +2,8 @@ class Stressor
   include Mongoid::Document
   field :name, :type => String
   field :description, :type => String
-  field :status, :type => String
+  field :status, :type => String # should be of several categories
+  field :mitigation_plan, type: String
+  # should have intensity over time
+
 end
