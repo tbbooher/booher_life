e5d02d9ac185d4500cbc125d2e4e067d811c654b
Tim Booher <ads@theboohers.org>
2012-04-14 21:53:23 -0400

    updated workout

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,6 +2,7 @@ class Workout
   include Mongoid::Document
   field :workout_date, :type => Date
   field :description, :type => String
-  field :power, :type => Integer
+  field :power, :type => Decimal
   field :crossfit_workout_id, :type => Integer
+  field :mileage, type: Decimal
 end
