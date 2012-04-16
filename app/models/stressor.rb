073fe5d2fdba2c1d55e685cf87a92b076fbb7948
Tim Booher <ads@theboohers.org>
2012-04-16 19:06:38 -0400

    Updated stressor fields

--- app/models/stressor.rb
+++ app/models/stressor.rb
@@ -2,8 +2,12 @@ class Stressor
   include Mongoid::Document
   field :name, :type => String
   field :description, :type => String
-  field :status, :type => String # should be of several categories
+  field :status, :type => Symbol # should be of several categories
   field :mitigation_plan, type: String
+  field :long_term_vision, type: Text
+  field :urgency, type: Symbol
+  field :impact, type: Symbol
+  field :difficulty, type: Symbol
   # should have intensity over time
 
   # i think a major point here is wether i can do anything about it
