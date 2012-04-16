f9ac833876025b8502b6382e9fed2ebe4cc9fd38
Tim Booher <ads@theboohers.org>
2012-04-15 22:51:53 -0400

    updated data fields to float

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,7 +2,7 @@ class Workout
   include Mongoid::Document
   field :workout_date, :type => Date
   field :description, :type => String
-  field :power, :type => Decimal
+  field :power, :type => Float
   field :crossfit_workout_id, :type => Integer
-  field :mileage, type: Decimal
+  field :mileage, type: Float
 end
