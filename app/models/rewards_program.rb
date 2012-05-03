75992a03871bb97dbcb6644a01cd44621395a0ef
Tim Booher <ads@theboohers.org>
2012-05-03 05:57:08 -0400

    added fields rewards programs

--- app/models/rewards_program.rb
+++ app/models/rewards_program.rb
@@ -4,4 +4,9 @@ class RewardsProgram
   field :miles, :type => Integer
   field :expiration_date, :type => Date
   field :number, :type => Integer
+  field :the_deal, type: String
+  field :website, type: String
+  field :username, type: String
+  field :password, type: String
+
 end
