8f8f49c4b04b8cc3e4bfd94225b3004bcd4c5338
Tim Booher <ads@theboohers.org>
2012-05-14 22:36:55 -0400

    update

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,8 @@
 class Goal
   include Mongoid::Document
+  include Mongoid::MultiParameterAttributes
+  include Mongoid::Timestamps
+
   field :name, type: String
   field :time_frame, :type => Symbol
   field :description, :type => String
